1abb3c34bb16ee6d8f7a66217f0d4d1f9f6aa1f0
Romain Altier <romain.altier@yahoo.fr>
2021-04-22 11:00:05 +0200

    Add Django files

---
+++ exercice_IA/interface_IA/views.py
@@ -0,0 +1,26 @@
+import os
+import pathlib
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import uuid
+from interface_IA.forms import UploadFileForm
+
+
+def index(request):
+    context = {
+        'form': UploadFileForm()
+    }
+    return render(request, 'interface_IA/home_page.html', context)
+
+
+def form_test(request):
+    form = UploadFileForm(request.POST, request.FILES)
+    file = request.FILES['file']
+    extension = pathlib.PureWindowsPath(file.name).suffix
+    file_name = str(uuid.uuid4()) + extension
+    file_path = os.getcwd() + '\\temp\\' + file_name
+    with open(file_path, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+    return HttpResponse("Hello, world. You're at the polls index.")
